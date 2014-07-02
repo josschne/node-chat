4f7e07e72628b953d4189c8e78558697a2831eed
josschne <josschne+codio@gmail.com>
2014-07-02 06:07:47 +0000

    Server to client working

---
+++ server.js
@@ -0,0 +1,8 @@
+
+var io = require('socket.io')(3000);
+
+console.log('server started!');
+io.on('connection', function(socket) {
+    console.log('client connected!');
+    setInterval(function() {socket.emit('message', 'hello!\n')}, 2000);
+});
