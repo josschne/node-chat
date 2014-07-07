cc10fbf7a36b51091d64b23b619927fc72363d48
josschne <josschne+codio@gmail.com>
2014-07-07 02:56:43 +0000

    Working chat both directions

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ var io = require('socket.io')(3000);
 console.log('server started!');
 io.on('connection', function(socket) {
     console.log('client connected!');
-    setInterval(function() {socket.emit('message', 'hello!\n')}, 2000);
-    socket.on('message', function(data) {console.log('Message: '+data)});
+    socket.on('message', function(data) {socket.broadcast.emit('message', data); console.log('Message: '+data);});
 });
