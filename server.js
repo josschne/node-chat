3f31d0eadfe5f6509fdafbe8f9c2586d759518e7
josschne <josschne+codio@gmail.com>
2014-07-07 01:57:18 +0000

    Static messages each way

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-
 var io = require('socket.io')(3000);
 
 console.log('server started!');
 io.on('connection', function(socket) {
     console.log('client connected!');
     setInterval(function() {socket.emit('message', 'hello!\n')}, 2000);
+    socket.on('message', function(data) {console.log('Message: '+data)});
 });
